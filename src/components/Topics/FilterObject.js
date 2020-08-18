9d3c4dc8f13661f8629c33c352a04065e7488ee0
Marshall Johnson <marshalljohnson193@gmail.com>
2020-08-18 16:34:58 -0600

    working on objects

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
 
 export default class FilterObject extends Component {
+    constructor(){
+        super()
+        this.state ={
+            unfilteredArray: [],
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    handleChange(value){
+        this.setState({unfilteredArray: [value]})
+    }
+
+
     render(){
         return(
-            <p>FilterObject Component</p>
+            <div className='puzzleBox filterObjectsPB'>
+                <h4>Filter Object</h4>
+                <span className="puzzleText"></span>
+                <input onChange={e => this.handleChange(e.target.value)} className="inputLine"/>
+                <button onClick={} className="confirmationButton"></button>
+                <span className="resultsBox filterObjectRB"></span>
+            </div>
         )
     }
 }
