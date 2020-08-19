5d630aa4d8ab840e8cc406b70f648f49305aa237
Marshall Johnson <marshalljohnson193@gmail.com>
2020-08-19 07:50:14 -0600

    finished the project

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -4,14 +4,41 @@ export default class FilterObject extends Component {
     constructor(){
         super()
         this.state ={
-            unfilteredArray: [],
+            employees: [
+                {
+                  name: 'Jimmy Joe',
+                  title: 'Hack0r',
+                  age: 12,
+                },
+                {
+                  name: 'Jeremy Schrader',
+                  age: 24,
+                  hairColor: 'brown'
+                },
+                {
+                  name: 'Carly Armstrong',
+                  title: 'CEO',
+                }
+              ],
+        
             userInput: '',
             filteredArray: []
         }
     }
 
     handleChange(value){
-        this.setState({unfilteredArray: [value]})
+        this.setState({userInput: value})
+    }
+
+    filterEmployees(prop) {
+        let employees = this.state.employees;
+        let filteredEmployees = [];
+
+        for ( let i = 0; i < employees.length; i++ ) {
+        if ( employees[i].hasOwnProperty(prop) ) {
+        filteredEmployees.push(employees[i]);
+        }
+    }
     }
 
 
@@ -19,9 +46,9 @@ export default class FilterObject extends Component {
         return(
             <div className='puzzleBox filterObjectsPB'>
                 <h4>Filter Object</h4>
-                <span className="puzzleText"></span>
+                <span className="puzzleText"> Original: { JSON.stringify(this.state.employees, null, 10) }</span>
                 <input onChange={e => this.handleChange(e.target.value)} className="inputLine"/>
-                <button onClick={} className="confirmationButton"></button>
+                <button onClick={() => this.filterEmployees(this.state.userInput)} className="confirmationButton"></button>
                 <span className="resultsBox filterObjectRB"></span>
             </div>
         )
